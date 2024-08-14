89ed356bcbf83323f8bd4cce2bbd3ac57dfd8191
ale155 <ale155@uclive.ac.nz>
2024-08-14 13:31:56 +1200

    Create the Edit FlashCard view model

---
+++ app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/viewmodels/EditCardViewModel.kt
@@ -0,0 +1,31 @@
+package nz.ac.canterbury.seng303.flashcardapp.viewmodels
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class EditCardViewModel:ViewModel() {
+    var question by mutableStateOf("")
+        private set
+    fun updateQuestion(newQuestion: String) {
+        question = newQuestion
+    }
+
+    var listAns = mutableStateListOf("","","","")
+        private set
+
+    fun updateAnswer(index: Int, newAnswer: String) {
+        if (index in listAns.indices) {
+            listAns[index] = newAnswer
+        }
+    }
+
+    var correctAns by mutableStateOf("")
+        private set
+
+    fun updateCorrectAnswer(newAnswer: String) {
+        correctAns = newAnswer
+    }
+}
