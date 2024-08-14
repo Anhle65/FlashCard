afadc5416495ba314bc9de2b76c2d5e0d9b5d909
ale155 <ale155@uclive.ac.nz>
2024-08-15 11:21:52 +1200

    Add the function for Add button

--- app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/CreateFlashCardScreen.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/CreateFlashCardScreen.kt
@@ -13,10 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -26,6 +28,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
@@ -53,14 +56,18 @@ fun CreateFlashCardScreen(navController: NavController,
             .fillMaxSize()
             .fillMaxHeight()
             .padding(16.dp)
+//            .verticalScroll(rememberScrollState())
             .drawBehind {
                 drawRoundRect(
                     color = Color.Cyan,
-                    cornerRadius = CornerRadius(15.dp.toPx()),
+                    cornerRadius = CornerRadius(16.dp.toPx()),
                 )
             }
             .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(16.dp)),
-        Arrangement.spacedBy(20.dp)
+//            .verticalScroll(true),
+        Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+//        a = Arrangement.Center
     ){
         Text(text = "Add a new flash card",
             textAlign = TextAlign.Center,
@@ -70,25 +77,29 @@ fun CreateFlashCardScreen(navController: NavController,
             ),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(25.dp)
+                .padding(10.dp)
         )
         OutlinedTextField(
             value = question,
             onValueChange = {onQuestionChange(it)},
             label = { Text("Input question here") },
             modifier = Modifier
-                .padding(horizontal = 20.dp)
+                .padding(16.dp)
                 .fillMaxWidth()
                 .drawBehind {
                     drawRoundRect(
                         color = Color.LightGray,
-                        cornerRadius = CornerRadius(15.dp.toPx()),
+                        cornerRadius = CornerRadius(16.dp.toPx()),
                     )
                 }
         )
         var checked by remember { mutableStateOf(listOf(mutableStateOf(false), mutableStateOf(false), mutableStateOf(false), mutableStateOf(false))) }
-        LazyColumn(modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(5.dp)) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+//                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
             items(listAns.size) { index ->
                 Log.d("Card Screen", "Answer $(listAns[index])")
 
@@ -96,7 +107,7 @@ fun CreateFlashCardScreen(navController: NavController,
                 Box(
                     modifier = Modifier
                         .fillMaxSize(),
-                    contentAlignment = androidx.compose.ui.Alignment.Center
+                    contentAlignment = Alignment.Center
                 ){
                     Row (
                         modifier = Modifier
@@ -119,7 +130,7 @@ fun CreateFlashCardScreen(navController: NavController,
                                 .drawBehind {
                                     drawRoundRect(
                                         color = Color.LightGray,
-                                        cornerRadius = CornerRadius(15.dp.toPx()),
+                                        cornerRadius = CornerRadius(16.dp.toPx()),
                                     )
                                 }
                         )
@@ -130,34 +141,66 @@ fun CreateFlashCardScreen(navController: NavController,
 
             }
         }
-        Button(
-            onClick = {
-                val builder = AlertDialog.Builder(context)
-                if (question != "" && listAnswer != emptyList<String>() && crrAns != "") {
-                    builder.setMessage("Created Card: $question")
-                        .setPositiveButton("Ok") { dialog, id -> /* Run some code on click */
-                            createCardFn(question, listAnswer, crrAns)
-                            onQuestionChange("")
-                            onCrrAnswerChange("")
-                            onListAnswerChange(emptyList<String>().toMutableList())
-                            navController.navigate("cardList")
-                        }.setNegativeButton("Cancel") { dialog, id ->
-                            dialog.dismiss()
-                        }
-                    val alert = builder.create()
-                    alert.show()
-                } else {
-                    Toast.makeText(context,"Could not create a flash card without question or content or an correct answer", Toast.LENGTH_SHORT).show()
-                }
-            },
-            modifier = Modifier
-                .fillMaxWidth(),
-        ) {
+//        Row (modifier = Modifier
+//            .padding(8.dp)
+//            .fillMaxWidth(),
+//            verticalAlignment = Alignment.CenterVertically,
+//            horizontalArrangement = Arrangement.Center,
+//        ){
+            Button(
+                onClick = {
+                    listAns = listAns.toMutableList().apply { add(mutableStateOf("")) }
+                    checked = checked.toMutableList().apply { add(mutableStateOf(false)) }},
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .padding(4.dp)
+                )
+            }
+//        }
+//        Row (modifier = Modifier
+//            .padding(8.dp),
+//            verticalAlignment = Alignment.CenterVertically,
+//            horizontalArrangement = Arrangement.Center
+//        ) {
+            Button(
+                onClick = {
+                    val builder = AlertDialog.Builder(context)
+                    if (question != "" && listAnswer != emptyList<String>() && crrAns != "") {
+                        builder.setMessage("Created Card: $question")
+                            .setPositiveButton("Ok") { dialog, id -> /* Run some code on click */
+                                createCardFn(question, listAnswer, crrAns)
+                                onQuestionChange("")
+                                onCrrAnswerChange("")
+                                onListAnswerChange(emptyList<String>().toMutableList())
+                                navController.navigate("cardList")
+                            }.setNegativeButton("Cancel") { dialog, id ->
+                                dialog.dismiss()
+                            }
+                        val alert = builder.create()
+                        alert.show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Could not create a flash card without question or content or an correct answer",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                },
+//                modifier = Modifier
+//                    .fillMaxWidth(),
+            ) {
 //            Icon(imageVector = Icons.Outlined.Create,
 //                contentDescription = "Save",
 //                tint = Color.Green
 //            )
-            Text(text = "Save")
+                Text(text = "Save and return")
+//            }
         }
     }
 }
