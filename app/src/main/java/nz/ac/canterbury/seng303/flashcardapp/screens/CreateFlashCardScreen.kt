14268e3e418c368c30c90e255130d91d7cd6b311
ale155 <ale155@uclive.ac.nz>
2024-08-15 16:41:31 +1200

    Add scrollable feature for screen

--- app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/CreateFlashCardScreen.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/CreateFlashCardScreen.kt
@@ -43,7 +43,7 @@ import androidx.navigation.NavController
 fun CreateFlashCardScreen(navController: NavController,
                           question: String,
                           onQuestionChange: (String) -> Unit,
-                          listAnswer: MutableList<String>,
+//                          listAnswer: MutableList<String>,
                           onListAnswerChange: (MutableList<String>) -> Unit,
                           onCrrAnswerChange: (String) -> Unit,
                           createCardFn: (String, MutableList<String>, String) -> Unit
@@ -51,7 +51,8 @@ fun CreateFlashCardScreen(navController: NavController,
     val context = LocalContext.current
     var crrAns = ""
     var listAns by rememberSaveable { mutableStateOf(listOf(mutableStateOf(""), mutableStateOf(""), mutableStateOf(""), mutableStateOf("")))}
-    Column (
+    var checked by remember { mutableStateOf(listOf(mutableStateOf(false), mutableStateOf(false), mutableStateOf(false), mutableStateOf(false))) }
+    LazyColumn (
         modifier = Modifier
             .fillMaxSize()
             .fillMaxHeight()
@@ -65,88 +66,75 @@ fun CreateFlashCardScreen(navController: NavController,
             }
             .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(16.dp)),
 //            .verticalScroll(true),
-        Arrangement.spacedBy(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-//        a = Arrangement.Center
     ){
-        Text(text = "Add a new flash card",
-            textAlign = TextAlign.Center,
-            style = TextStyle(
-                color = Color.Black,
-                fontSize = MaterialTheme.typography.headlineLarge.fontSize
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-        OutlinedTextField(
-            value = question,
-            onValueChange = {onQuestionChange(it)},
-            label = { Text("Input question here") },
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-                .drawBehind {
-                    drawRoundRect(
-                        color = Color.LightGray,
-                        cornerRadius = CornerRadius(16.dp.toPx()),
-                    )
-                }
-        )
-        var checked by remember { mutableStateOf(listOf(mutableStateOf(false), mutableStateOf(false), mutableStateOf(false), mutableStateOf(false))) }
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth(),
-//                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(5.dp)
-        ) {
-            items(listAns.size) { index ->
-                Log.d("Card Screen", "Answer $(listAns[index])")
-
-//                var changedAns by rememberSaveable { mutableStateOf(listAns[index]) }
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ){
-                    Row (
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                    ){
-                        Checkbox(
-                            checked = checked[index].value,
-                            onCheckedChange = { checked[index].value = it
-                            crrAns = listAns[index].value}
+        item {
+            Text(text = "Add a new flash card",
+                textAlign = TextAlign.Center,
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = MaterialTheme.typography.headlineLarge.fontSize
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+        }
+        item {
+            OutlinedTextField(
+                value = question,
+                onValueChange = {onQuestionChange(it)},
+                label = { Text("Input question here") },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .drawBehind {
+                        drawRoundRect(
+                            color = Color.LightGray,
+                            cornerRadius = CornerRadius(16.dp.toPx()),
                         )
-                        Log.d("Card Screen", "Correct ans is $crrAns")
-                        OutlinedTextField(
-                            value = listAns[index].value,
-                            onValueChange = {listAns[index].value = it},
-                            label = { Text("Empty answer now") },
-                            modifier = Modifier
-                                .padding(horizontal = 5.dp)
-                                .drawBehind {
-                                    drawRoundRect(
-                                        color = Color.LightGray,
-                                        cornerRadius = CornerRadius(16.dp.toPx()),
-                                    )
-                                }
-                        )
-                        Log.e("Create", "changed value $listAns")
                     }
-
+            )
+        }
+        items(listAns.size) { index ->
+            Log.d("Card Screen", "Answer $(listAns[index])")
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Row (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                ){
+                    Checkbox(
+                        checked = checked[index].value,
+                        onCheckedChange = { checked[index].value = it
+                        crrAns = listAns[index].value}
+                    )
+                    Log.d("Card Screen", "Correct ans is $crrAns")
+                    OutlinedTextField(
+                        value = listAns[index].value,
+                        onValueChange = {listAns[index].value = it},
+//                                        listAnswer[index] = it},
+                        label = { Text("Empty answer now") },
+                        modifier = Modifier
+                            .padding(horizontal = 5.dp)
+                            .drawBehind {
+                                drawRoundRect(
+                                    color = Color.LightGray,
+                                    cornerRadius = CornerRadius(16.dp.toPx()),
+                                )
+                            }
+                        )
+                    Log.e("Create", "changed value $listAns")
                 }
-
             }
         }
-//        Row (modifier = Modifier
-//            .padding(8.dp)
-//            .fillMaxWidth(),
-//            verticalAlignment = Alignment.CenterVertically,
-//            horizontalArrangement = Arrangement.Center,
-//        ){
+        item {
             Button(
                 onClick = {
                     listAns = listAns.toMutableList().apply { add(mutableStateOf("")) }
@@ -162,19 +150,36 @@ fun CreateFlashCardScreen(navController: NavController,
                         .padding(4.dp)
                 )
             }
-//        }
-//        Row (modifier = Modifier
-//            .padding(8.dp),
-//            verticalAlignment = Alignment.CenterVertically,
-//            horizontalArrangement = Arrangement.Center
-//        ) {
+        }
+        item {
             Button(
+                modifier = Modifier
+                    .padding(8.dp),
                 onClick = {
                     val builder = AlertDialog.Builder(context)
-                    if (question != "" && listAnswer != emptyList<String>() && crrAns != "") {
+                    if (question == "") {
+                        Toast.makeText(
+                            context,
+                            "Could not create a flash card without question",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else if (listAns == emptyList<String>()) {
+                        Toast.makeText(
+                            context,
+                            "Could not create a flash card without answer",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    } else if (crrAns == "") {
+                        Toast.makeText(
+                            context,
+                            "Could not create a flash card without correct answer",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
                         builder.setMessage("Created Card: $question")
                             .setPositiveButton("Ok") { dialog, id -> /* Run some code on click */
-                                createCardFn(question, listAnswer, crrAns)
+                                createCardFn(question,listAns.toMutableList(), crrAns)
                                 onQuestionChange("")
                                 onCrrAnswerChange("")
                                 onListAnswerChange(emptyList<String>().toMutableList())
@@ -184,23 +189,11 @@ fun CreateFlashCardScreen(navController: NavController,
                             }
                         val alert = builder.create()
                         alert.show()
-                    } else {
-                        Toast.makeText(
-                            context,
-                            "Could not create a flash card without question or content or an correct answer",
-                            Toast.LENGTH_SHORT
-                        ).show()
                     }
                 },
-//                modifier = Modifier
-//                    .fillMaxWidth(),
-            ) {
-//            Icon(imageVector = Icons.Outlined.Create,
-//                contentDescription = "Save",
-//                tint = Color.Green
-//            )
+            ){
                 Text(text = "Save and return")
-//            }
+            }
         }
     }
 }
