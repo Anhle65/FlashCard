4aa91eb291d82962d94cd11117e6a59d0dd2f7eb
ale155 <ale155@uclive.ac.nz>
2024-08-16 11:30:29 +1200

    Add the current number question over total number and submit button

--- app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/FlashCardScreen.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/FlashCardScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
@@ -38,79 +40,95 @@ import nz.ac.canterbury.seng303.flashcardapp.viewmodels.FlashCardViewModel
 
 @Composable
 fun FlashCardScreen(cardId: String, cardViewModel: FlashCardViewModel){
-
+    cardViewModel.getCards()
+    val listCards: List<FlashCard> by cardViewModel.cards.collectAsState(emptyList())
+    var totalQuestion = listCards.size
+    var currentQuestion by rememberSaveable { mutableStateOf(1)}
     cardViewModel.getCardById(cardId = cardId.toIntOrNull())
     val selectedCardState by cardViewModel.selectedCard.collectAsState(null)
     val card: FlashCard? = selectedCardState
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
-            .fillMaxSize()
-            .drawBehind {
-                drawRoundRect(
-                    color = Color.Cyan,
-                    cornerRadius = CornerRadius(15.dp.toPx()),
-                )
-            }
-            .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(16.dp)),
-        verticalArrangement = Arrangement.spacedBy(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Play flash cards",
-            style = MaterialTheme.typography.headlineLarge,
+    if (card != null) {
+        var listAnswers by remember { mutableStateOf(card.listAnswer.toMutableList()) }
+        LazyColumn(
             modifier = Modifier
-                .padding(16.dp))
-        if (card != null) {
-            Box(modifier = Modifier
-                .fillMaxWidth()
                 .padding(16.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .fillMaxSize()
                 .drawBehind {
                     drawRoundRect(
-                        color = Color.LightGray,
-                        cornerRadius = CornerRadius(10.dp.toPx())
-                    )}
-                .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(16.dp))
-            ){
-                Text(text = "${card.question}",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-//                        .drawBehind {
-//                            drawRoundRect(
-//                                color = Color.LightGray,
-//                                cornerRadius = CornerRadius(10.dp.toPx())
-//                            )
-//                        }
-                )
-            }
-            var listAnswers by remember { mutableStateOf(card.listAnswer.toMutableList()) }
-            LazyColumn {
-                items (listAnswers.size) { index ->
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp)
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.spacedBy(20.dp),
-                    ) {
-                        var selectedAnswer by rememberSaveable { mutableStateOf("") }
-                        RadioButton(
-                            selected = selectedAnswer == listAnswers[index],
-                            onClick = { selectedAnswer = listAnswers[index] },
-                            colors = RadioButtonDefaults.colors(
-                                selectedColor = MaterialTheme.colorScheme.primary,
-                                unselectedColor = Color.Gray
-                            )
+                        color = Color.Cyan,
+                        cornerRadius = CornerRadius(16.dp.toPx()),
+                    )
+                }
+                .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(16.dp)),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item { Text(text = "Play flash cards",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier
+                    .padding(16.dp)) }
+            item {
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .drawBehind {
+                        drawRoundRect(
+                            color = Color.LightGray,
+                            cornerRadius = CornerRadius(10.dp.toPx())
                         )
-                        Text(text = card.listAnswer[index])
-                        Log.d("Flash card screen", "selected answer: $selectedAnswer")
                     }
-                     // Add a divider between items
+                    .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(10.dp))
+                ){
+                    Text(text = "${card.question}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    )
+                }
+//                    var listAnswers by remember { mutableStateOf(card.listAnswer.toMutableList()) }
+            }
+            items (listAnswers.size) { index ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp),
+                ) {
+                    var selectedAnswer by rememberSaveable { mutableStateOf("") }
+                    RadioButton(
+                        selected = selectedAnswer == listAnswers[index],
+                        onClick = { selectedAnswer = listAnswers[index] },
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = MaterialTheme.colorScheme.primary,
+                            unselectedColor = Color.Gray
+                        )
+                    )
+                    Text(text = card.listAnswer[index])
+                    Log.d("Flash card screen", "selected answer: $selectedAnswer")
                 }
             }
-        } else {
-            Text(text = "Could not find card: $cardId", style = MaterialTheme.typography.headlineMedium)
+            item {
+                Row (modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp),
+                    verticalAlignment = Alignment.CenterVertically.apply { Alignment.BottomEnd }
+                )
+                {
+                    Text(text = "$currentQuestion/$totalQuestion")
+                    Button(
+                        onClick = { currentQuestion += 1
+                        }) {
+                        Text(text = "Submit")
+                    }
+                }
+            }
         }
+    } else {
+        Text(text = "Could not find card: $cardId", style = MaterialTheme.typography.headlineMedium)
     }
 }
