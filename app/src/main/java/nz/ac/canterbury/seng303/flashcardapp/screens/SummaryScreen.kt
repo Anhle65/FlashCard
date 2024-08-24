76f8f36c9db4da63c881409be2dec8d72fc9d83b
ale155 <ale155@uclive.ac.nz>
2024-08-24 15:16:54 +1200

    Show the player's result on the SummaryScreen after he plays all cards in list

--- app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/SummaryScreen.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/screens/SummaryScreen.kt
@@ -1,15 +1,30 @@
 package nz.ac.canterbury.seng303.flashcardapp.screens
 
+import android.util.Log
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
@@ -22,29 +37,93 @@ import nz.ac.canterbury.seng303.flashcardapp.viewmodels.FlashCardViewModel
 
 @Composable
 fun SummaryScreen (navController: NavController, cardViewModel: FlashCardViewModel) {
-    Column (
+    val flashCards = cardViewModel.results.keys.toList()
+    val answers = cardViewModel.results.values.toList()
+    LazyColumn(
         modifier = Modifier
-            .fillMaxSize()
+            .padding(horizontal = 16.dp)
             .fillMaxWidth()
             .fillMaxHeight()
-            .padding(16.dp)
+            .fillMaxSize()
             .drawBehind {
                 drawRoundRect(
                     color = Color.Cyan,
                     cornerRadius = CornerRadius(16.dp.toPx()),
                 )
             }
-            .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(16.dp)),){
-    }
-        Text(text = "${cardViewModel.playerName}'s summary results",
-            textAlign = TextAlign.Center,
-            style = TextStyle(
-                color = Color.Black,
-                fontSize = MaterialTheme.typography.headlineLarge.fontSize
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
-        )
+            .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(16.dp)),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            Text(
+                text = "${cardViewModel.playerName}'s Summary",
+                textAlign = TextAlign.Center,
+                style = TextStyle(
+                    color = Color.Blue,
+                    fontSize = MaterialTheme.typography.headlineLarge.fontSize
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+            )
+        }
+        item {
+            Text(
+                text = "Score: ${cardViewModel.counterCorrect}/${cardViewModel.totalQuestion}",
+                textAlign = TextAlign.Center,
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = MaterialTheme.typography.headlineLarge.fontSize
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+            )
+        }
 
+        Log.d("SUMMARY", "$flashCards wwith $answers")
+        items(flashCards.size) { index->
+            Card (modifier = Modifier
+                .padding(horizontal = 25.dp, vertical = 5.dp)
+                .fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+            ){
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                ) {
+                    Text(
+                        text = flashCards[index].question,
+                        style = TextStyle(
+                            color = Color.Black
+                        ),
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 8.dp)
+                    )
+                    if (!answers[index]) {
+                        Icon(
+                            modifier = Modifier
+                                .size(48.dp)
+                                .padding(horizontal = 5.dp),
+                            imageVector = Icons.Outlined.Close,
+                            contentDescription = "Wrong Answer",
+                            tint = Color.Red
+                        )
+                    } else {
+                        Icon(
+                            modifier = Modifier
+                                .size(48.dp)
+                                .padding(horizontal = 5.dp),
+                            imageVector = Icons.Outlined.Check,
+                            contentDescription = "Correct Answer",
+                            tint = Color.Green
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
