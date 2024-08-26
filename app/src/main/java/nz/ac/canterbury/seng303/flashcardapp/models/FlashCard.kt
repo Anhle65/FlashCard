018e282f69d442a6f6ad23b381bad21a217265be
ale155 <ale155@uclive.ac.nz>
2024-08-27 10:29:27 +1200

    Remove companion object

--- app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/models/FlashCard.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/models/FlashCard.kt
@@ -5,30 +5,6 @@ data class FlashCard (
     val question: String,
     val listAnswer: MutableList<String>,
     val correctAnswer: String): Identifiable {
-    companion object {
-        fun getCards(): List<FlashCard> {
-            return listOf(
-                FlashCard(
-                    1,
-                    "Is today wednesday",
-                    mutableListOf("True", "False"," Not sure"),
-                    "true",
-                ),
-                FlashCard(
-                    2,
-                    "What is the color",
-                    mutableListOf("Blue", "Green", "Yellow"),
-                    "Green"
-                ),
-                FlashCard(
-                    3,
-                    "Where is the cat",
-                    mutableListOf("Sofa", "Bedroom", "Don't know", "Kitchen"),
-                    "Sofa"
-                )
-            )
-        }
-    }
     override fun getIdentifier(): Int {
         return id
     }
