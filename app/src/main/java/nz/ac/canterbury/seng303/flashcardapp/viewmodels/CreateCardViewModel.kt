0d146778697d4a40b02efb7988e705b2e92f65fd
ale155 <ale155@uclive.ac.nz>
2024-08-26 13:43:52 +1200

    Remove redundant import

--- app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/viewmodels/CreateCardViewModel.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/flashcardapp/viewmodels/CreateCardViewModel.kt
@@ -1,12 +1,7 @@
 package nz.ac.canterbury.seng303.flashcardapp.viewmodels
-import android.util.Log
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 class CreateCardViewModel: ViewModel() {
     var question by mutableStateOf("")
